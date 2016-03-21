a8783cfebb95a19da65a32d5c3e4b2db76b620ee
RealoFoxtrot <darkhollybob@gmail.com>
2016-03-21 17:08:14 +0000

    Player Finder Imported (OBJExport Deleted)
    FindCharacter: Added
    FindCharacter: Insert the Player into the public Slot in the editor, and
    the Pointer rotates to aim at where the player is
    KittenTest: Basic Prim Arrow added. Placement needs testing
    WARNING
    Elina has Deleted OBJExport on her project. Be aware of this, incase it
    get deleted on yours

---
+++ Assets/FindCharacter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FindCharacter : MonoBehaviour {
+
+    private Rigidbody rb;
+    public GameObject player;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Vector3 playerLocation = player.transform.position;
+        Vector3 PointAtPlayer = playerLocation - transform.position;
+
+        Quaternion Finder = Quaternion.LookRotation(PointAtPlayer);
+
+        rb.MoveRotation(Finder);
+	}
+}
