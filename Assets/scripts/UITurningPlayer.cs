aa01a6ee63036d45a6fd6e84435c5027879b8ca6
RealoFoxtrot <darkhollybob@gmail.com>
2016-04-05 13:02:08 +0100

    Oculus 1.3 & Gameplay Update
    Branch Created to try out the Oculus 1.3 update, and make sure
    everything is working.
    
    Gameplay changes: Main ones are that the Level's scale has been dropped
    in order to create a feeling of being inside it. Hasn't quite worked, so
    will be playing around with it still

--- Assets/scripts/UITurningPlayer.cs
+++ Assets/scripts/UITurningPlayer.cs
@@ -1,28 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class UITurning : MonoBehaviour {
+public class UITurningPlayer : MonoBehaviour
+{
 
     public GameObject Camera;
 
     private int lives;
     private Vector3 cameraLocation;
     private Vector3 pointAtCamera;
-	
-	// Update is called once per frame
-	void Update () {
 
-        lives = GetComponentInParent<SimpleAgent>().lives;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        lives = GetComponentInParent<PinballMovement>().lives;
 
 
         GetComponent<TextMesh>().text = "Lives: " + lives;
-	}
+    }
 
     void FixedUpdate()
     {
         cameraLocation = Camera.transform.position;
 
 
-        transform.LookAt( 2 * transform.position - cameraLocation) ;
+        transform.LookAt(2 * transform.position - cameraLocation);
     }
 }
